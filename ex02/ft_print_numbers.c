da6326fa8ca641085cdf38f30709c26bd1593b11
sibusiso nkosi <sbnkosi@c4r2s3.wethinkcode.co.za>
2020-06-21 13:56:23 +0200

    numbers

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -6,18 +6,23 @@
 /*   By: sbnkosi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 08:41:48 by sbnkosi           #+#    #+#             */
-/*   Updated: 2020/06/21 11:34:13 by sbnkosi          ###   ########.fr       */
+/*   Updated: 2020/06/21 13:55:59 by sbnkosi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_print_numbers(void)
 {
 	char numbers= '0';
 	while(numbers <= '9')
 	{
-		write(1, &numbers, 1);
+		ft_putchar(numbers);
 		numbers++;
 	}
 	write(1, "\n", 1);
