6584ce900216917d87c85c487399d7703112f208
sibusiso nkosi <sbnkosi@c4r2s3.wethinkcode.co.za>
2020-06-21 13:53:41 +0200

    alphabet

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,19 +6,24 @@
 /*   By: sbnkosi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 08:18:33 by sbnkosi           #+#    #+#             */
-/*   Updated: 2020/06/21 11:26:39 by sbnkosi          ###   ########.fr       */
+/*   Updated: 2020/06/21 13:51:58 by sbnkosi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void 	ft_putchar(char a)
+{
+	write(1, &c, 1);
+}
 
 void	ft_print_reverse_alphabet(void)
 {
 	char z = 'z';
 	while(z >= 'a')
 	{
-		write(1, &z, 1);
+		ft_putchar(z);
 	   z--;	
 	}
-	write(1, "\n", 1);
+	ft_putchar(z);
 }
